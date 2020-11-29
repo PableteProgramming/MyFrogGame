dc663e25aa13f20b0e609980e7900d998b263020
PableteProgramming <pabletegym@icloud.com>
2020-11-29 20:52:09 +0100

    Camera follows player and background added

---
+++ Assets/Scripts/AllFruitsEat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AllFruitsEat : MonoBehaviour
+{
+    void Update()
+    {
+        AllFruitsCollected();
+    }
+
+    public void AllFruitsCollected()
+    {
+        if (transform.childCount == 0)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                ChangeScene();
+            }
+        }
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
