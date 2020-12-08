a22fe0e154da9728b8ba251c7d002110a468e2a1
PableteProgramming <pabletegym@icloud.com>
2020-12-08 20:32:09 +0100

    Two sides platforms done...but...when want to go down from one, the other also change direction

---
+++ Assets/Scripts/Platforms/TwoSidesPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoSidesPlatform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s"))
+        {
+            effector.rotationalOffset = 180f;
+        }
+
+        if (Input.GetKeyDown("space") || Input.GetKeyDown("up"))
+        {
+            effector.rotationalOffset = 0f;
+        }
+    }
+}
