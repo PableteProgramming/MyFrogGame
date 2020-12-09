b22abf8d03afe91f4cbef5a5948f1ccfca21c7a3
PableteProgramming <pabletegym@icloud.com>
2020-12-09 19:08:03 +0100

    Angry pig prefab created

--- Assets/Scripts/Enemies/AngryPig/Move.cs
+++ Assets/Scripts/Enemies/AngryPig/Move.cs
@@ -62,4 +62,12 @@ public class Move : MonoBehaviour
         }
         waitedTime += Time.deltaTime;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.GetComponent<PlayerRespawn>().PlayerDamage();
+        }
+    }
 }
