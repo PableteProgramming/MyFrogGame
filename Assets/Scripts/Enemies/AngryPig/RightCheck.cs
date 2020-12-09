de18b3f37937557c5b021fb580079d535328e959
PableteProgramming <pabletegym@icloud.com>
2020-12-09 17:53:33 +0100

    Adding angry pig

---
+++ Assets/Scripts/Enemies/AngryPig/RightCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightCheck : MonoBehaviour
+{
+    public static bool IsWalled = false;
+    private void Start()
+    {
+        IsWalled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            IsWalled = true;
+            //Debug.Log(IsWalled);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            IsWalled = false;
+            //Debug.Log(IsWalled);
+        }
+    }
+}
--- Assets/Scripts/Platforms/TwoSidesPlatform.cs
+++ Assets/Scripts/Platforms/TwoSidesPlatform.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TwoSidesPlatform : MonoBehaviour
 {
     private PlatformEffector2D effector;
+    public bool IsPlayed;
     void Start()
     {
         effector = GetComponent<PlatformEffector2D>();
@@ -14,7 +15,10 @@ public class TwoSidesPlatform : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s"))
         {
-            effector.rotationalOffset = 180f;
+            if (IsPlayed)
+            {
+                effector.rotationalOffset = 180f;
+            }
         }
 
         if (Input.GetKeyDown("space") || Input.GetKeyDown("up"))
@@ -22,4 +26,21 @@ public class TwoSidesPlatform : MonoBehaviour
             effector.rotationalOffset = 0f;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            IsPlayed = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            IsPlayed = false;
+        }
+    }
+
 }
