5b2246462fbee231c3006a52a5b2bd5b9c155673
PableteProgramming <pabletegym@icloud.com>
2020-12-18 18:29:17 +0100

    Adding options menu

---
+++ Assets/Scripts/Options/OptionsMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public string MainMenuScene;
+
+    public void Options()
+    {
+        Time.timeScale = 0;
+        optionsPanel.SetActive(true);
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1;
+        optionsPanel.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(MainMenuScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
