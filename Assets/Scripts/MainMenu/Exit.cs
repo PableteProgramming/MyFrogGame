9aa9ca0e5f05b415699c86631cadce32ae850325
PableteProgramming <pabletegym@icloud.com>
2020-12-23 19:08:27 +0100

    Adding chiquimono's doors and mainmenu of worlds and sound with copyrights changed

---
+++ Assets/Scripts/MainMenu/Exit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    private bool inDoor = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //text.gameObject.SetActive(true);
+            inDoor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //text.gameObject.SetActive(false);
+        inDoor = false;
+    }
+
+    private void Update()
+    {
+        if (inDoor && Input.GetKey(KeyCode.Space))
+        {
+            Application.Quit();
+        }
+    }
+}
