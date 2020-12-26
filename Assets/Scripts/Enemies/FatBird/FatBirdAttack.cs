82b3244d231bc53ec68e423dca0ab2ceea5be67f
PableteProgramming <pabletegym@icloud.com>
2020-12-26 20:32:16 +0100

    FatBird fall add but does not go up again yet

---
+++ Assets/Scripts/Enemies/FatBird/FatBirdAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatBirdAttack : MonoBehaviour
+{
+    public bool Saw;
+    public GameObject RayCastEndPoint;
+    private float distanceRaycast;
+
+    private void Start()
+    {
+        distanceRaycast = Vector2.Distance(RayCastEndPoint.transform.position, transform.position);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit2d = Physics2D.Raycast(transform.position, Vector2.down, distanceRaycast);
+        if (hit2d.collider != null)
+        {
+            if (hit2d.collider.CompareTag("Player"))
+            {
+                Saw = true;
+            }
+            else
+            {
+                Saw = false;
+            }
+        }
+        else
+        {
+            Saw = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/FatBird/FatBirdMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatBirdMove : MonoBehaviour
+{
+    private bool Dynamic;
+    public Rigidbody2D rb2d;
+    public Animator animator;
+    public float UpSpeed=0.5f;
+    public GameObject RayCastSpawnPoint;
+
+    private void Update()
+    {
+
+        if (RayCastSpawnPoint.GetComponent<FatBirdAttack>().Saw)
+        {
+            Dynamic = true;
+            rb2d.bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        if (Dynamic)
+        {
+            if (rb2d.velocity.y < 0)
+            {
+                animator.SetBool("Falling", true);
+                animator.SetBool("Grounded", false);
+            }
+            else
+            {
+                animator.SetBool("Falling", false);
+                animator.SetBool("Grounded", true);
+            }
+        }
+    }
+}
