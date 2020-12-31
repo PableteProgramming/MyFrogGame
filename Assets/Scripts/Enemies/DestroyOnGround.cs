8611ce5c451e65e5396e7760ba6143620f491231
PableteProgramming <pabletegym@icloud.com>
2020-12-31 18:43:14 +0100

    Plant ennemy done

---
+++ Assets/Scripts/Enemies/DestroyOnGround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnGround : MonoBehaviour
+{
+    public float DestroyTime;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            Destroy(gameObject, DestroyTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            Destroy(gameObject, DestroyTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Plant/BulletMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public float speed;
+    public bool left;
+
+    private void Update()
+    {
+        if (left)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+
+}
