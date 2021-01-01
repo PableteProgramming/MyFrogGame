c3c2c81d6381b09ca82027ed469b6c8435876003
PableteProgramming <pabletegym@icloud.com>
2021-01-01 20:04:55 +0100

    Move script changed form rigidbody to transform.Translate

--- Assets/Scripts/Enemies/Move.cs
+++ Assets/Scripts/Enemies/Move.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public Rigidbody2D rb2d;
     public SpriteRenderer sprite;
     public GameObject Leftcheck;
     public GameObject Rightcheck;
     public float walkspeed;
     public float realwalkspeed;
-    public float waitime;
-    public float waitedTime;
+    private float waitime;
+    private float waitedTime;
     private bool sleeping;
     private bool moving;
 
@@ -19,46 +18,23 @@ public class Move : MonoBehaviour
     {
         sleeping = false;
         moving = true;
+        waitime = 0;
         waitedTime = 0;
         realwalkspeed = walkspeed;
     }
 
     void Update()
     {
-        
-        if (waitedTime >= waitime)
-        {
-            if (sleeping)
-            {
-                sleeping = false;
-                moving = true;
-                waitedTime = 0;
-            }
-            else if (moving)
-            {
-                sleeping = true;
-                moving = false;
-                waitedTime = 0;
-            }
-        }
-
         if (Leftcheck.GetComponent<Check>().IsWalled)
         {
             walkspeed = -realwalkspeed;
             sprite.flipX = true;
-            waitime = 0;
         }
         else if (Rightcheck.GetComponent<Check>().IsWalled)
         {
             sprite.flipX = false;
             walkspeed = realwalkspeed;
-            waitime = 0;
         }
-
-        if (moving)
-        {
-            rb2d.velocity = new Vector2(-walkspeed, rb2d.velocity.y);
-        }
-        waitedTime += Time.deltaTime;
+        transform.Translate(Vector2.right*(-walkspeed)*Time.deltaTime);
     }
 }
