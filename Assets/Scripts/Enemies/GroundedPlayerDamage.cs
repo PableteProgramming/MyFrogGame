3583bbd4202b52710ed13836f533aca36fdb5a02
PableteProgramming <pabletegym@icloud.com>
2021-01-23 16:17:09 +0100

    RockHead prefab created

--- Assets/Scripts/Enemies/GroundedPlayerDamage.cs
+++ Assets/Scripts/Enemies/GroundedPlayerDamage.cs
@@ -6,15 +6,37 @@ public class GroundedPlayerDamage : MonoBehaviour
 {
 
     public GameObject PlayerCheckGround;
+    public bool setParent;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            collision.transform.SetParent(transform);
             if (PlayerCheckGround.GetComponent<CheckGround>().Ground)
             {
                 collision.transform.GetComponent<PlayerRespawn>().PlayerDamage();
             }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+            if (PlayerCheckGround.GetComponent<CheckGround>().Ground)
+            {
+                collision.transform.GetComponent<PlayerRespawn>().PlayerDamage();
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
         }
     }
 }
